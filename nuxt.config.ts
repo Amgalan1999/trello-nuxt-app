789e1a87ac7cf94b539da077450d328479180017
Amgalan1999 <48343569+Amgalan1999@users.noreply.github.com>
2023-01-03 23:58:53 +0800

    tailwind and app main config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            title: 'Trello',
+            titleTemplate: '%s • Personal clone app',
+            charset: 'utf-8',
+            viewport: 'width=devide-width, initial-scale=1',
+            meta: [
+                { hid: "description", name: "description", content: "" },
+            ]
+        }
+    },
 
+    modules: ['@nuxtjs/tailwindcss'],
+
+    tailwindcss: {
+        cssPath: '~/assets/css/tailwind.css',
+        configPath: '~/tailwind.config.js',
+        exposeConfig: false,
+        injectPosition: 0,
+        viewer: true,
+    },
 })
